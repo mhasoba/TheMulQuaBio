0af2c092d0c4cc110316c4f6ff350c04f0409fec
Samraat Pawar <mhasoba@gmail.com>
2017-11-14 14:28:06 +0000

    Updated jupyter nbs

--- silbiocomp/Practicals/Code/blackbirds.py
+++ silbiocomp/Practicals/Code/blackbirds.py
@@ -12,12 +12,11 @@ text = text.replace('\n',' ')
 # note that there are "strange characters" (these are accents and
 # non-ascii symbols) because we don't care for them, first transform
 # to ASCII:
-text = text.decode('ascii', 'ignore')
+text = text.decode('ascii', 'ignore') #will not work in python 3
 
-# Now write a regular expression my_reg that captures # the Kingdom, 
-# Phylum and Species name for each species and prints it out neatly:
-
-# my_reg = ??????
+# Now extend this script so that it captures the Kingdom, 
+# Phylum and Species name for each species and prints it out to screen neatly.
 
 # Hint: you may want to use re.findall(my_reg, text)...
 # Keep in mind that there are multiple ways to skin this cat! 
+# Your solution may involve multiple regular expression calls (easier!), or a single one (harder!)
