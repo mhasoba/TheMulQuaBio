8ba69b031d3f9e6b739cd134eb294f46d44d080a
Samraat Pawar <mhasoba@gmail.com>
2017-12-01 13:41:57 +0000

    Updated notes, added HPC jupyter nb

--- silbiocomp/Practicals/Code/MyHPCScript.py
+++ silbiocomp/Practicals/Code/MyHPCScript.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 02 16:20:48 2015
+Created on Wed Nov 02 16:20:48 2017
 
 @author: Samraat Pawar
 
 """
-import os # need this to get environment variables
+import os # to get environment variables
+
+home <- os.getenv('HOME')
 
 i = int(os.getenv("PBS_ARRAY_INDEX"))
 
+####Functions block start ####
+def do_simulation(ar1, arg2, etc):
+	results = ...
+	return results
+#### Functions block end ####
+
 do_simulation(i)
 
-def do_simulation():
-	...
-	return ...
+save(results, file='home/MyProject/results_HPC.csv')
