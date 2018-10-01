6cf8e1d8e2fcfbdfcbe1caaeb01646d3e27a43d4
Samraat Pawar <mhasoba@gmail.com>
2018-10-01 08:37:58 +0100

    Updated Jupyter nbs for the 2018-19 academic year

--- code/boilerplate.sh
+++ code/boilerplate.sh
@@ -3,7 +3,7 @@
 # Script: boilerplate.sh
 # Desc: simple boilerplate for shell scripts
 # Arguments: none
-# Date: Oct 2015
+# Date: Oct 2018
 
 echo -e "\nThis is a shell script! \n" #what does -e do?
 
