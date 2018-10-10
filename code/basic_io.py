110e54c22b617b8e19069f44212ccee43a13a803
Samraat Pawar <mhasoba@gmail.com>
2018-10-10 09:26:25 +0100

    Updated  python I chapter notebook

--- code/basic_io.py
+++ code/basic_io.py
@@ -2,7 +2,7 @@
 # FILE INPUT
 #############################
 # Open a file for reading
-f = open('../sandbox/test.txt', 'r')
+f = open('../sandbox/test.txt', 'r') 
 # use "implicit" for loop:
 # if the object is a file, python will cycle over lines
 for line in f:
