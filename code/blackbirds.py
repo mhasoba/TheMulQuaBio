2a98551133a3a47b2fd08a319eb263f226b9f5e3
Samraat Pawar <mhasoba@gmail.com>
2018-11-14 09:49:38 +0000

    updated Python II chapter

--- code/blackbirds.py
+++ code/blackbirds.py
@@ -1,22 +1,23 @@
 import re
 
-# Read the file
-f = open('../Data/blackbirds.txt', 'r')
-text = f.read()
-f.close()
+# Read the file (using a different, more python 3 way, just for fun!)
+with open('../data/blackbirds.txt', 'r') as f:
+    text = f.read()
 
-# remove \t\n and put a space in:
+# replace \t's and \n's with a spaces:
 text = text.replace('\t',' ')
 text = text.replace('\n',' ')
+# You may want to make other changes to the text. 
 
-# note that there are "strange characters" (these are accents and
-# non-ascii symbols) because we don't care for them, first transform
-# to ASCII:
-text = text.decode('ascii', 'ignore') #will not work in python 3
+# In particular, note that there are "strange characters" (these are accents and
+# non-ascii symbols) because we don't care for them, first transform to ASCII:
 
-# Now extend this script so that it captures the Kingdom, 
-# Phylum and Species name for each species and prints it out to screen neatly.
+text = text.encode('ascii', 'ignore') # first encode into ascii bytes
+text = text.decode('ascii', 'ignore') # Now decode back to string
 
-# Hint: you may want to use re.findall(my_reg, text)...
-# Keep in mind that there are multiple ways to skin this cat! 
-# Your solution may involve multiple regular expression calls (easier!), or a single one (harder!)
+# Now extend this script so that it captures the Kingdom, Phylum and Species
+# name for each species and prints it out to screen neatly.
+
+# Hint: you may want to use re.findall(my_reg, text)... Keep in mind that there
+# are multiple ways to skin this cat! Your solution could involve multiple
+# regular expression calls (easier!), or a single one (harder!)
