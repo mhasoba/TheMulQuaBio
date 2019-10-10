d8481c54f9a9d44b4328f98514923364486126ee
Samraat Pawar <mhasoba@gmail.com>
2019-10-10 11:56:32 +0100

    Edits to Python I chapter and code

--- code/cfexercises1.py
+++ code/cfexercises1.py
@@ -1,27 +1,37 @@
-# How many times will 'hello' be printed?
-for i in range(3, 17):
-    print('hello')
+# What does each of fooXX do? 
+def foo1(x):
+    return x ** 0.5
 
-for j in range(12):
-    if j % 3 == 0:
-        print('hello')
+def foo2(x, y):
+    if x > y:
+        return x
+    return y
 
-for j in range(15):
-     if j % 5 == 3:
-        print('hello')
-     elif j % 4 == 3:
-        print('hello')
+def foo3(x, y, z):
+    if x > y:
+        tmp = y
+        y = x
+        x = tmp
+    if y > z:
+        tmp = z
+        z = y
+        y = tmp
+    return [x, y, z]
 
-z = 0
-while z != 15:
-    print('hello')
-    z = z + 3
+def foo4(x):
+    result = 1
+    for i in range(1, x + 1):
+        result = result * i
+    return result
 
-z = 12
-while z < 100:
-    if z == 31:
-        for k in range(7):
-            print 'hello'
-    elif z == 18:
-        print 'hello'
-    z = z + 1
+def foo5(x): # a recursive function that calculates the factorial of x
+    if x == 1:
+        return 1
+    return x * foo5(x - 1)
+     
+def foo6(x): # Calculate the factorial of x in a different way
+    facto = 1
+    while x >= 1:
+        facto = facto * x
+        x = x - 1
+    return facto
