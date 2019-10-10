cec26de635f14240445e61e65535d6bc2c12da3c
Samraat Pawar <mhasoba@gmail.com>
2019-10-10 08:54:01 +0100

    Edits to Python I chapter

--- code/using_name.py
+++ code/using_name.py
@@ -5,3 +5,5 @@ if __name__ == '__main__':
     print('This program is being run by itself')
 else:
     print('I am being imported from another module')
+
+print("This module's name is: " + __name__)
