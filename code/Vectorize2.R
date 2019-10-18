3abf84a4821effde69acdd2c06cf51e6be54a604
Samraat Pawar <mhasoba@gmail.com>
2019-10-18 11:11:58 +0100

    Updated R data chapter

--- code/Vectorize2.R
+++ code/Vectorize2.R
@@ -12,7 +12,7 @@ stochrick<-function(p0=runif(1000,.5,1.5),r=1.2,K=1,sigma=0.2,numyears=100)
     
     for (yr in 2:numyears){ #for each pop, loop through the years
 
-      N[yr,pop] <- N[yr-1,pop] * exp(r * (1-N[yr-1,pop]/K) + rnorm(1,0,sigma))
+      N[yr,pop] <- N[yr-1,pop] * exp(r * (1 - N[yr - 1,pop] / K) + rnorm(1,0,sigma))
     
     }
   
