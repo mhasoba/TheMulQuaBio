38b3e10f06484d765be6e1167ba0ef7263fc9f52
Samraat Pawar <mhasoba@gmail.com>
2019-10-18 09:05:45 +0100

    Updated R chapters and added some missing data flies

--- code/Vectorize2.R
+++ code/Vectorize2.R
@@ -8,12 +8,14 @@ stochrick<-function(p0=runif(1000,.5,1.5),r=1.2,K=1,sigma=0.2,numyears=100)
   N<-matrix(NA,numyears,length(p0))
   N[1,]<-p0
   
-  for (pop in 1:length(p0)) #loop through the populations
-  {
-    for (yr in 2:numyears) #for each pop, loop through the years
-    {
-      N[yr,pop]<-N[yr-1,pop]*exp(r*(1-N[yr-1,pop]/K)+rnorm(1,0,sigma))
+  for (pop in 1:length(p0)){#loop through the populations
+    
+    for (yr in 2:numyears){ #for each pop, loop through the years
+
+      N[yr,pop] <- N[yr-1,pop] * exp(r * (1-N[yr-1,pop]/K) + rnorm(1,0,sigma))
+    
     }
+  
   }
  return(N)
 
