2d441b11e00426c7d8a72c80cb0846cb5aff1872
Samraat Pawar <mhasoba@gmail.com>
2019-11-15 14:38:04 +0000

    Updated Python II and jupyter intro nb

--- code/Nets.R
+++ code/Nets.R
@@ -6,15 +6,12 @@ rm(list = ls())
 # ICL	0	0	10	9	5	70
 # UoR		0	12	0	2	76
 # CEH			0	0	0	6
-# ZSL				0# plot(net, edge.arrow.size=1, edge.curved=.1,
-#      vertex.color="orange", vertex.frame.color="#555555",
-#      vertex.label=V(net)$Type, vertex.label.color="black",
-#      vertex.label.cex=.7) 	0	28
-# CEFAS					0	0
-# Nonacademic/CASE						0
+# ZSL			       0 	0	28
+# CEFAS			       0	0
+# Nonacademic/CASE                        0
 
-links <- read.csv("../Data/QMEE_Net_Mat_edges.csv", header=T, as.is=T)
-nodes <- read.csv("../Data/QMEE_Net_Mat_nodes.csv", header=T, row.names = 1)
+links <- read.csv("../data/QMEE_Net_Mat_edges.csv", header=T, as.is=T)
+nodes <- read.csv("../data/QMEE_Net_Mat_nodes.csv", header=T, row.names = 1)
 
 #Create graph object
 net <- graph.adjacency(as.matrix(links), mode = "directed", weighted=TRUE, diag=F)
@@ -45,11 +42,11 @@ E(net)$width <- 1+E(net)$weight/10
 
 graphics.off()
 
-svg("../Results/QMEENet.svg",width=7,height=7)
+svg("../results/QMEENet.svg",width=7,height=7)
 
 plot(net, edge.curved=0, vertex.label.color="black") 
 
-legend(x=-1.5, y=-0.1, c("University","Hosting Partner", "Non-hosting Partner"), pch=21,
+legend(x=-1.5, y=-0.1, c("Hosting Partner", "Non-hosting Partner", "University"), pch=21,
        col="#777777", pt.bg=colrs, pt.cex=2, cex=.8, bty="n", ncol=1)
 
 dev.off()
--- code/fmr.R
+++ code/fmr.R
@@ -1,6 +1,5 @@
 # Plots log(field metabolic rate) against log(body mass) for the Nagy et al 
 # 1999 dataset to a file fmr.pdf.
-# Writes the list of species names to species.csv
 
 cat("Reading CSV\n")
 
