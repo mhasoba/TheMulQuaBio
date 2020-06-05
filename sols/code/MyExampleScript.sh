8e94497967907008c24063bc19830abca808fc25
Samraat Pawar <mhasoba@gmail.com>
2020-06-05 07:40:19 +0100

    Edits to stats Jupyter nbs, removed solutions directory

--- sols/code/MyExampleScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-msg1="Hello"
-msg2=$USER
-echo "$msg1 $msg2"
-
-echo "Hello $USER"
-echo
-
--- sols/code/using_name.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Filename: using_name.py
-
-if __name__ == '__main__':
-    print('This program is being run by itself')
-else:
-    print('I am being imported from another module')
-
-print("This module's name is: " + __name__)
