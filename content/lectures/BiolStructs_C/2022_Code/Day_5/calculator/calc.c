48def918fa22888a6545f0b07821077f01f9c650
mbrazeau <martin.brazeau@gmail.com>
2022-01-14 16:12:49 +0000

    add the calculator program

---
+++ content/lectures/BiolStructs_C/2022_Code/Day_5/calculator/calc.c
@@ -0,0 +1,19 @@
+float add_float(float a, float b)
+{
+    return a + b;
+}
+
+float sub_float(float a, float b)
+{
+    return a - b;
+}
+
+float mult_float(float a, float b)
+{
+    return a * b;
+}
+
+float div_float(float a, float b)
+{
+    return a / b;
+}
